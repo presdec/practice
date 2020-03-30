d0c4eb7e1b3e8c73e81369f51b75b647cfc85d3f
Philip Prescott-Decie <presdec@gmail.com>
2020-03-31 00:07:29 +0300

    Moer Exercises

---
+++ HackerRank_solutions/Problem Solving/Pciking Numbers/solution.py
@@ -0,0 +1,39 @@
+# https://www.hackerrank.com/challenges/picking-numbers/problem
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    maximum = 0
+
+    for k in a:
+        n1 = a.count(k)
+        n2 = a.count(k-1)
+        maximum = max(maximum, n1+n2)
+    return maximum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
